501dff15aa994d9c3747b41c753a78e9bc2e05d8
youngkim90 <ceo.youngkim@gmail.com>
2022-02-21 23:08:36 +0900

    로그추적기 필드 동기화 사용

---
+++ src/main/java/hello/advanced/app/v3/OrderServiceV3.java
@@ -0,0 +1,28 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV3 {
+
+    private final OrderRepositoryV3 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.request()");    //추적 레벨 증가
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;    //예외를 던져주어야 한다.
+        }
+    }
+}
---
+++ src/main/java/hello/advanced/trace/logtrace/LogTrace.java
@@ -0,0 +1,12 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceStatus;
+
+public interface LogTrace {
+
+    TraceStatus begin(String meassage);
+
+    void end(TraceStatus status);
+
+    void exception(TraceStatus status, Exception e);
+}
